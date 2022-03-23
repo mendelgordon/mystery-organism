2b8426f1982f9e8cd7ff014c04bb8bafa87559a9
Mendel Gordon <mendelgordons@gmail.com>
2022-03-23 19:12:35 -0400

    Add missing r

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const returnRandBase = () => {
   return dnaBases[rng(4)];
 };
 
-// Returns a random single stand of DNA containing 15 bases
+// Returns a random single strand of DNA containing 15 bases
 const mockUpStrand = () => {
   const newStrand = [];
   for (let i = 0; i < 15; i++) {
